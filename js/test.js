147bd1e44427eb003dd5ffce65d40c912f17256a
Erich Loftis <erichlof@swbell.net>
2022-10-18 16:16:36 -0500

    Update test.js

--- js/test.js
+++ js/test.js
@@ -3,11 +3,21 @@
 // 	console.log('All assets are loaded');
 // });
 
+console.log("Hello from the JavaScript file!");
+
 let canvas = document.getElementById('myCanvas');
 
 let context = canvas.getContext('2d');
 
 let pixelRatio = 0.5;
+
+let rectW = 10;
+let rectH = 10;
+let rectPosX = 0;
+let rectPosY = 0;
+let r = 255, g = 0, b = 0;
+
+let colorString = "";//"rgb(" + r + ", " + g + "," + b + ")";
 
 
 function add(num1, num2)
@@ -16,6 +26,33 @@ function add(num1, num2)
 }
 
 window.addEventListener('resize', handleWindowResize);
+window.addEventListener('orientationchange', handleWindowResize);
+
+window.addEventListener('keydown', handleKeyDown);
+function handleKeyDown(event)
+{
+	if (event.code == 'ArrowUp')
+	{
+		rectPosY -= 1;
+		handleWindowResize();
+	}
+
+	if (event.code == 'ArrowDown')
+	{
+		rectPosY += 1;
+		handleWindowResize();
+	}
+	if (event.code == 'ArrowLeft')
+	{
+		rectPosX -= 1;
+		handleWindowResize();
+	}
+	if (event.code == 'ArrowRight')
+	{
+		rectPosX += 1;
+		handleWindowResize();
+	}
+}
 
 
 let messageElement = document.getElementById('message');
@@ -27,7 +64,7 @@ webpageResolutionElement.innerHTML = "webpage w: " + window.innerWidth + " h: " 
 let canvasResolutionElement = document.getElementById("canvasResolution");
 
 let sliderElement = document.getElementById('slider');
-sliderElement.type = 'range';
+//sliderElement.type = 'range';
 sliderElement.min = 0.5;
 sliderElement.max = 1.0;
 sliderElement.step = 0.05;
@@ -36,6 +73,8 @@ sliderElement.addEventListener('change', handleWindowResize);
 sliderElement.addEventListener('pointermove', handleWindowResize);
 sliderElement.addEventListener('pointerdown', handleGrabbing);
 sliderElement.addEventListener('pointerup', handleRelease);
+sliderElement.addEventListener('touchstart', handleWindowResize);
+sliderElement.addEventListener('touchmove', handleWindowResize);
 function handleGrabbing()
 {
 	sliderElement.style.cursor = "grabbing";
@@ -67,15 +106,22 @@ function handleWindowResize()
 	context.stroke();
 	context.closePath();
 
-	context.fillStyle = 'yellow';
-	context.fillRect(canvas.width - 10, canvas.height - 10, 10, 10);
+	r = Math.floor(Math.random() * 256);
+	g = Math.floor(Math.random() * 256);
+	b = Math.floor(Math.random() * 256);
 
-	context.fillStyle = 'white';
-	context.fillRect(canvas.width / 2, canvas.height / 2, 10, 10);
+	colorString = "rgb(" + r + "," + g + "," + b + ")";
+
+	context.fillStyle = colorString;
+	context.fillRect(canvas.width - 10, canvas.height - 10, rectW, rectH);
+
+	context.fillStyle = colorString;
+	context.fillRect(canvas.width / 2 + rectPosX, canvas.height / 2 + rectPosY, rectW, rectH);
 	
 
 	messageElement.innerHTML = "Resolution: " + pixelRatio;
 
+	webpageResolutionElement.innerHTML = "webpage w: " + window.innerWidth + " h: " + window.innerHeight;
 	canvasResolutionElement.innerHTML = "canvas w: " + canvas.width + " h: " + canvas.height;
 
 }
